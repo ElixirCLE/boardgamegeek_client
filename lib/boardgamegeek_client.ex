6ad3e0d5c4fecae97291a2a55b7e7e562de2ad09
Mathew Bramson <mathewbramson@gmail.com>
2016-10-12 21:10:14 -0400

    Refactor board_game_geek_client code
    add base case for get_games_info.
    
    add unary get_games_info function
    
    switch binary get_games_info paramter order so accumulator is last
    
    add game_from_xml method

--- lib/boardgamegeek_client.ex
+++ lib/boardgamegeek_client.ex
@@ -10,22 +10,31 @@ defmodule BoardGameGeekClient do
     response = BoardGameGeek.post(url)
     doc = get_response(url, response, response.status_code)
     game_ids = Exml.get doc, "//items/item/@objectid"
-    get_games_info([], game_ids)
+    get_games_info(game_ids)
   end
 
-  defp get_games_info(games_list, [head | tail]) do
+  defp get_games_info(game_ids), do: get_games_info(game_ids, [])
+  defp get_games_info([], games_acc), do: games_acc
+  defp get_games_info([head | tail], games_acc) do
     url = "thing?id=" <> head
     response = BoardGameGeek.post(url)
     doc = get_response(url, response, response.status_code)
-    name = Exml.get doc, "//items/item/name[@type='primary']/@value"
-    min_players = Exml.get doc, "//items/item/minplayers/@value"
-    max_players = Exml.get doc, "//items/item/maxplayers/@value"
+    game = game_from_xml(doc)
 
     # We need to add the sleep because making requests too fast will cause BGG to close the connection
     # Ideal solution: Just get the game names/ids, then start a background process to update min/max players
     :timer.sleep(500)
-    get_games_info(games_list ++ [%Game{name: name, min_players: String.to_integer(min_players),
-                   max_players: String.to_integer(max_players)}], tail)
+
+    get_games_info(tail, [game | games_acc])
+  end
+
+  defp game_from_xml(doc) do
+    name = Exml.get doc, "//items/item/name[@type='primary']/@value"
+    min_players = Exml.get doc, "//items/item/minplayers/@value"
+    max_players = Exml.get doc, "//items/item/maxplayers/@value"
+    %Game{name: name,
+          min_players: String.to_integer(min_players),
+          max_players: String.to_integer(max_players)}
   end
 
   defp get_games_info(games_list, []) do
