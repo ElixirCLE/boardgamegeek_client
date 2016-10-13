86b7d77db525aea712e4bd7bdcd7b34a88326587
Mathew Bramson <mathewbramson@gmail.com>
2016-10-12 22:14:48 -0400

    Refactor get_response
    add unary function head to get_response that accepts just the url
    
    change :timer.sleep/1 to Process.sleep/1
    
    move initial BoardGameGeek.post call before calling get_response
    to the unary get_response method.

--- lib/boardgamegeek_client.ex
+++ lib/boardgamegeek_client.ex
@@ -7,8 +7,7 @@ defmodule BoardGameGeekClient do
 
   def get_game_collection(username) do
     url = "collection?username=" <> username <> "&own=1&subtype=boardgame"
-    response = BoardGameGeek.post(url)
-    doc = get_response(url, response, response.status_code)
+    doc = get_response(url)
     game_ids = Exml.get doc, "//items/item/@objectid"
     get_games_info(game_ids)
   end
@@ -17,13 +16,12 @@ defmodule BoardGameGeekClient do
   defp get_games_info([], games_acc), do: games_acc
   defp get_games_info([head | tail], games_acc) do
     url = "thing?id=" <> head
-    response = BoardGameGeek.post(url)
-    doc = get_response(url, response, response.status_code)
+    doc = get_response(url)
     game = game_from_xml(doc)
 
     # We need to add the sleep because making requests too fast will cause BGG to close the connection
     # Ideal solution: Just get the game names/ids, then start a background process to update min/max players
-    :timer.sleep(500)
+    Process.sleep(500)
 
     get_games_info(tail, [game | games_acc])
   end
@@ -37,8 +35,13 @@ defmodule BoardGameGeekClient do
           max_players: String.to_integer(max_players)}
   end
 
+  defp get_response(url) do
+    response = BoardGameGeek.post(url)
+    get_response(url, response, response.status_code)
+  end
+
   defp get_response(url, _response, 202) do
-    :timer.sleep(500)
+    Process.sleep(500)
     response = BoardGameGeek.post(url)
     get_response(url, response, response.status_code)
   end
