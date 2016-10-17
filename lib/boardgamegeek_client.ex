9c5d8c4d7d2b73b8c3a11321bf3148cd80f8d9b3
Mathew Bramson <mathewbramson@gmail.com>
2016-10-17 16:11:57 -0400

    Refactor BoardGameGeekClient to properly retry
    Rather than retrying a single query until BoardGameGeek has responded
    with the data we need, the logic has been refactored to add that game to
    the end of the games list so that it is retried after trying the other
    games first.

--- lib/boardgamegeek_client.ex
+++ lib/boardgamegeek_client.ex
@@ -5,25 +5,35 @@ defmodule BoardGameGeekClient do
 
   require BoardGameGeek
 
-  def get_game_collection(username) do
-    url = "collection?username=" <> username <> "&own=1&subtype=boardgame"
-    doc = get_response(url)
+  def get_game_collection(username, wait \\ 500) do
+    url = "collection?username=#{username}&own=1&subtype=boardgame"
+    doc = get_response(url, wait)
     game_ids = Exml.get doc, "//items/item/@objectid"
+    Process.sleep(wait)
     get_games_info(game_ids)
   end
 
-  def get_games_info(game_ids) when is_list(game_ids), do: get_games_info(game_ids, [])
-  defp get_games_info([], games_acc), do: games_acc
-  defp get_games_info([head | tail], games_acc) do
-    url = "thing?id=" <> head
-    doc = get_response(url)
-    game = game_from_xml(doc)
+  def get_games_info(game_ids, wait \\ 500) when is_list(game_ids), do: get_games_info(game_ids, wait, [])
+  defp get_games_info([], _wait, acc), do: acc
+  defp get_games_info([head|tail], wait, acc) do
+    url = "thing?id=#{head}"
+    response = BoardGameGeek.post(url)
+    case response.status_code do
+      202 ->
+        Process.sleep(wait)
+        # Results not ready, append to end of games list so that it retries
+        get_games_info(tail ++ [head], wait, acc)
+      200 ->
+        Process.sleep(wait)
+        get_games_info(tail, wait, [game_from_response(response) | acc])
+      true -> raise "unknown status code: #{response.status_code}"
+    end
+  end
 
-    # We need to add the sleep because making requests too fast will cause BGG to close the connection
-    # Ideal solution: Just get the game names/ids, then start a background process to update min/max players
-    Process.sleep(500)
-
-    get_games_info(tail, [game | games_acc])
+  defp game_from_response(response) do
+    response.body
+    |> Exml.parse
+    |> game_from_xml
   end
 
   defp game_from_xml(doc) do
@@ -35,18 +45,18 @@ defmodule BoardGameGeekClient do
           max_players: String.to_integer(max_players)}
   end
 
-  defp get_response(url) do
+  defp get_response(url, wait) do
     response = BoardGameGeek.post(url)
-    get_response(url, response, response.status_code)
+    get_response(url, wait, response, response.status_code)
   end
 
-  defp get_response(url, _response, 202) do
-    Process.sleep(500)
+  defp get_response(url, wait, _response, 202) do
+    Process.sleep(wait)
     response = BoardGameGeek.post(url)
-    get_response(url, response, response.status_code)
+    get_response(url, wait, response, response.status_code)
   end
 
-  defp get_response(_url, response, _status_code) do
+  defp get_response(_url, _wait, response, _status_code) do
     response.body
     |> Exml.parse
   end
