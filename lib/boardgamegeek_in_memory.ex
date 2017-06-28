8bff33ab1f34bf1c393db13ba97cae1bd7e428cd
Chris Rees <crees@covermymeds.com>
2017-06-28 09:25:25 -0400

    Add regression doctests around a search with spaces in the name and a search that returns a single result

--- lib/boardgamegeek_in_memory.ex
+++ lib/boardgamegeek_in_memory.ex
@@ -4,6 +4,11 @@ defmodule BoardGameGeek.InMemory do
     %HTTPotion.Response{body: body}
   end
 
+  def get("search?query=Sushi+Go+Party&type=boardgame", _) do
+    body = File.read!("test/fixtures/search_games_single_result_fixture.xml")
+    %HTTPotion.Response{body: body}
+  end
+
   def get("collection?username=Serneum&own=1&subtype=boardgame", _) do
     body = File.read!("test/fixtures/get_collection_fixture.xml")
     %HTTPotion.Response{body: body}
