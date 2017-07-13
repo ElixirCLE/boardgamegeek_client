5434b6836d0e3762793588682e66c1c28d6672dd
Chris Rees <crees@covermymeds.com>
2017-07-13 18:28:14 -0400

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BoardGameGeekClient.Mixfile do
   def project do
     [
       app: :boardgamegeek_client,
-      description: description,
+      description: description(),
       version: "0.1.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
@@ -34,6 +34,7 @@ defmodule BoardGameGeekClient.Mixfile do
   defp deps do
     [
       {:credo, "~> 0.4", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:exml, "~> 0.1"},
       {:httpotion, "~> 3.0.0"},
       {:poison, "~> 2.0"},
